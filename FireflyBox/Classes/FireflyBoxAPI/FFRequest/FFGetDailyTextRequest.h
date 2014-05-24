1300c094bf22ac9e92696f99edc899b29ca9c980
pig <pig@miglab.com>
2014-05-24 17:46:59 +0800

    add modify

---
+++ FireflyBox/Classes/FireflyBoxAPI/FFRequest/FFGetDailyTextRequest.h
@@ -0,0 +1,13 @@
+//
+//  FFGetDailyTextRequest.h
+//  FireflyBox
+//
+//  Created by pig on 14-5-24.
+//  Copyright (c) 2014年 pig. All rights reserved.
+//
+
+#import "FFBaseRequest.h"
+
+@interface FFGetDailyTextRequest : FFBaseRequest
+
+@end
---
+++ FireflyBox/Classes/FireflyBoxAPI/FFRequest/FFGetDailyTextRequest.m
@@ -0,0 +1,17 @@
+//
+//  FFGetDailyTextRequest.m
+//  FireflyBox
+//
+//  Created by pig on 14-5-24.
+//  Copyright (c) 2014年 pig. All rights reserved.
+//
+
+#import "FFGetDailyTextRequest.h"
+
+@implementation FFGetDailyTextRequest
+
+
+
+
+
+@end
